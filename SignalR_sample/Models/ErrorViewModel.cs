403a107db2478a0dd61b7a0633b86f50f517d063
Ali_Jafari <alijafari0572@gmail.com>
2023-11-17 13:14:04 +0330

    Add project files.

---
+++ SignalR_sample/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SignalR_sample.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
